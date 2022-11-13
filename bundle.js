e6c1527152f54962329268d566cdca351524db17
lies <lies@MacBookvonlies.fritz.box>
2022-11-13 20:25:12 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -116,22 +116,27 @@
 
     //const colors = new Float32Array([]);
 
-    const spinnenNetz = function (vertexDataObject) {
-        const m = 5;
+    const pillow = function (vertexDataObject) {
+        const m = 7;
         const n = 32;
         const vertices = new Float32Array(3 * (n + 1) * (m + 1));
         const indices = new Uint16Array(2 * 2 * n * m);
 
-        const dt = (2 * Math.PI) / n;
-        const dr = 1 / m;
+        const umin = 0;
+        const umax = Math.PI;
+        const vmin = -1 * Math.PI;
+        const vmax = Math.PI;
+        const a = 0.5;
+        const du = (umin + umax) / n;
+        const dv = (vmin - vmax) / m;
         let iIndex = 0;
 
-        for (let i = 0, t = 0; i <= n; i++, t += dt) {
-            for (let j = 0, r = 0; j <= m; j++, r += dr) {
+        for (let i = 0, u = 0; i <= n; i++, u += du) {
+            for (let j = 0, v = 0; j <= m; j++, v += dv) {
                 let iVertex = i * (m + 1) + j;
-                let x = r * Math.cos(t);
-                let y = r * Math.sin(t);
-                let z = 0;
+                let x = Math.cos(u);
+                let z = Math.cos(v);
+                let y = a * Math.sin(u) * Math.sin(v);
                 vertices[iVertex * 3] = x;
                 vertices[iVertex * 3 + 1] = y;
                 vertices[iVertex * 3 + 2] = z;
@@ -150,8 +155,49 @@
         vertexDataObject.indices = indices;
     };
 
-    const vertexDataSpider = createVertexData(spinnenNetz);
-    createVertexData(function (vertexDataObject) {});
+    const dinis = function (vertexDataObject) {
+        const n = 32;
+        const m = 7;
+        const c = 0.17;
+        const vertices = new Float32Array(3 * (n + 1) * (m + 1));
+        const indices = new Uint16Array(2 * 2 * n * m);
+
+        const du = (4 * Math.PI) / n;
+        const dv = (2 - 0.01) / n;
+        const a = 3;
+        const b = 0.6;
+
+        let iIndex = 0;
+
+        for (let i = 0, u = 0; i <= n; i++, u += du) {
+            for (let j = 0, v = 0.01; j <= m; j++, v += dv) {
+                let iVertex = i * (m + 1) + j;
+                let x = a * Math.cos(u) * Math.sin(v) * c;
+                let z = a * Math.sin(u) * Math.sin(v) * c;
+                let y =
+                    (a * (Math.cos(v) + Math.log(Math.tan(v * 0.5))) + b * u) * c;
+
+                vertices[iVertex * 3] = x;
+                vertices[iVertex * 3 + 1] = y;
+                vertices[iVertex * 3 + 2] = z;
+
+                if (j > 0 && i > 0) {
+                    indices[iIndex++] = iVertex - 1;
+                    indices[iIndex++] = iVertex;
+                }
+
+                if (j > 0 && i > 0) {
+                    indices[iIndex++] = iVertex - (m + 1);
+                    indices[iIndex++] = iVertex;
+                }
+            }
+        }
+        vertexDataObject.vertices = vertices;
+        vertexDataObject.indices = indices;
+    };
+
+    const vertexDataSpider = createVertexData(pillow);
+    const vertexDataDinis = createVertexData(dinis);
 
     const gl = initContext("gl_context");
     const gl2 = initContext("gl_context_02");
@@ -172,7 +218,7 @@
     );
     initBuffer(
         gl2,
-        vertexDataSpider.vertices,
+        vertexDataDinis.vertices,
         gl2.ARRAY_BUFFER,
         initObject2.program,
         "pos",
@@ -193,23 +239,37 @@
     gl2.bindBuffer(gl2.ELEMENT_ARRAY_BUFFER, ibo2);
     gl2.bufferData(
         gl2.ELEMENT_ARRAY_BUFFER,
-        vertexDataSpider.indices,
+        vertexDataDinis.indices,
         gl2.STATIC_DRAW
     );
     ibo2.numberOfElements = vertexDataSpider.indices.length;
 
     gl.clearColor(0.95, 0.95, 0.95, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.frontFace(gl.CCW);
+    gl.enable(gl.CULL_FACE);
+    gl.cullFace(gl.BACK);
     gl.drawElements(gl.LINE_STRIP, ibo.numberOfElements, gl.UNSIGNED_SHORT, 0);
 
     gl2.clearColor(0.95, 0.95, 0.95, 1);
     gl2.clear(gl2.COLOR_BUFFER_BIT);
-    gl2.drawElements(gl2.LINE_STRIP, ibo2.numberOfElements, gl2.UNSIGNED_SHORT, 0);
+    gl2.frontFace(gl2.CCW);
+    gl2.enable(gl2.CULL_FACE);
+    gl2.cullFace(gl2.BACK);
+    gl2.drawElements(gl2.LINES, ibo2.numberOfElements, gl2.UNSIGNED_SHORT, 0);
 
     gl3.clearColor(0.95, 0.95, 0.95, 1);
-    gl3.clear(gl.COLOR_BUFFER_BIT);
+    gl3.clear(gl3.COLOR_BUFFER_BIT);
+    gl3.frontFace(gl3.CCW);
+    gl3.enable(gl3.CULL_FACE);
+    gl3.cullFace(gl3.BACK);
+    gl3.drawElements(gl3.LINES, ibo2.numberOfElements, gl3.UNSIGNED_SHORT, 0);
 
     gl4.clearColor(0.95, 0.95, 0.95, 1);
-    gl4.clear(gl.COLOR_BUFFER_BIT);
+    gl4.clear(gl4.COLOR_BUFFER_BIT);
+    gl4.frontFace(gl4.CCW);
+    gl4.enable(gl4.CULL_FACE);
+    gl4.cullFace(gl4.BACK);
+    gl4.drawElements(gl4.LINES, ibo2.numberOfElements, gl2.UNSIGNED_SHORT, 0);
 
 })();
